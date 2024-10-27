7c40a0e76562bf454ad16b8fdff8a603a6dac5be
Mohamed Dawood <mohamedbasyonidawood23@gmail.com>
2024-10-27 14:15:19 +0300

    implement navbar in responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,25 @@
 import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import Star from '../images/Star.png';
 import Profile from '../images/Profile.png';
+import { useState } from 'react';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleOpenNav() {
+    setIsOpen(() => !isOpen);
+    console.log(isOpen);
+  }
+
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
           travel <span>goo</span>
         </div>
-        <ul>
+        <ul className={!isOpen ? 'active' : ''}>
           <li>Home</li>
           <li>About Us</li>
           <li>Packages</li>
@@ -22,10 +32,12 @@ function Navbar() {
             <img className="profile" src={Profile} alt="" />
           </button>
         </div>
-        <div className="bars">
-          <span>-</span>
-          <span>-</span>
-          <span>-</span>
+        <div
+          className="bars"
+          style={{ borderColor: !isOpen ? 'var(--main-color)' : 'transparent' }}
+          onClick={handleOpenNav}
+        >
+          <FontAwesomeIcon icon={faBars} />
         </div>
       </div>
     </div>
